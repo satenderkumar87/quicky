a46611606c934297888e36ccd1a6d601d5b4b3b5
Tarun Kumar <tarun.kumar.dev@outlook.com>
2025-07-19 13:45:34 +0530

    Update project structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Intial React App</h1>
-      </header>
+      {/* <header className="App-header"> */}
+        {/* <h1>Intial React App</h1> */}
+        <Home />
+      {/* </header> */}
     </div>
   );
 }
